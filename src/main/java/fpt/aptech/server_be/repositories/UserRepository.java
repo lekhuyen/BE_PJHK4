f1e922979c30a2a834c06c81e32391442c647bb2
lekhuyen <127919257+lekhuyen@users.noreply.github.com>
2024-11-12 11:33:03 +0700

    cmm3

--- src/main/java/fpt/aptech/server_be/repositories/UserRepository.java
+++ src/main/java/fpt/aptech/server_be/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package fpt.aptech.server_be.repositories;
 
 import fpt.aptech.server_be.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmail(String email);
 }
